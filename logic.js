32043944101c1eda4760b9dc9b0773eb0eb75aae
GAMES2018 <35645038+ktung2018@users.noreply.github.com>
2018-08-08 17:50:18 -0700

    Add files via upload

--- logic.js
+++ logic.js
@@ -1,4 +1,70 @@
-﻿//first data query url 
+﻿//create map
+
+var apiKey = "pk.eyJ1IjoidHVuZ2siLCJhIjoiY2ppZHo4OXIwMGd0NDNrcGkzZDV0ZXE2eCJ9.t1Xpklyzo_R5_G8bJ_ygxg";
+
+var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.light",
+  accessToken: apiKey
+});
+
+var satellitemap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets-satellite",
+  accessToken: apiKey
+});
+
+var outdoormap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.outdoors",
+  accessToken: apiKey
+});
+
+// We then create the map object with options. Adding the tile layers we just
+// created to an array of layers.
+var map = L.map("mapid", {
+  center: [40.7, -94.5],
+  zoom: 3,
+  layers: [darkmap, satellitemap, outdoormap]
+});
+
+// Adding our 'graymap' tile layer to the map.
+darkmap.addTo(map);
+
+
+
+// We create the layers for our two different sets of data, earthquakes and tectonicplates.
+var tectonicplates = new L.LayerGroup();
+var earthquakes = new L.LayerGroup();
+
+// Defining an object that contains all of our different map choices. Only one
+// of these maps will be visible at a time!
+var baseMaps = {
+  Satellite: satellitemap,
+  Darkmap: darkmap,
+  Outdoors: outdoormap
+};
+
+// We define an object that contains all of our overlays. Any combination of
+// these overlays may be visible at the same time!
+var overlayMaps = {
+  "Tectonic Plates": tectonicplates,
+  Earthquakes: earthquakes
+};
+
+// Then we add a control to the map that will allow the user to change which
+// layers are visible.
+L.control
+  .layers(baseMaps, overlayMaps)
+  .addTo(map);
+
+
+/*********************
+  
+//first data query url 
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson"
 
 //second data Tectonic plates url
@@ -96,6 +162,23 @@ function createMap(earthquakes){
 
 }; 
 
+//Create legend
+var legend = L. control({position:'bottomright'});
+  legend.onAdd = function (map){
+    var div = L.DomUtil.create('div', 'info legend'),
+              grades = [0, 1, 2, 3, 4, 5], 
+              labels = [];
+
+  //loop thru intervals and create label
+    for (var i=0; i<grades.length; i++){
+      div.innerHTML +=
+      '<i style="background:' + getColor(grades[i] +1) + "></i>" +
+      grades[i] + (grades[i+1] ? '&ndash;' + grades[i+1] + '<br>': '+');
+    }
+    return div;
+  };
+
+  legend.addTo(map);
   //color of marker based on magnitude of the earthquake
   getColor(mag){
     switch (true){
@@ -121,3 +204,4 @@ function getRadius(mag){
   }
   return mag*4;
 }
+**************/
